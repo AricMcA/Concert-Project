2efe7bbe5304ceef3dea91b7959193545fb55cce
Aric McAlister <aricmca@gmail.com>
2022-03-17 15:46:03 -0500

    adding Reply resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :replies
+  resources :posts
   resources :users
   resources :concerts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
