d7da9944462078a1ab848753cd5442c532e532d9
Aric McAlister <aricmca@gmail.com>
2022-03-23 13:53:07 -0500

    adding user authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  
   resources :replies
   resources :posts
   resources :users
   resources :concerts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
 end
